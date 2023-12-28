42a138b8d5812f11fc2cca9aa229d95880897459
FrankGrillo1991 <fsg12491@aol.com>
2023-12-28 07:51:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const ratingCard = document.querySelector('.ratings-container');
-const thankCard = document.querySelector('.thank-you-container');
-const submitButton = document.querySelector('rating');
-const rating = document.getElementById('rating');
-const ratingNumber = document.querySelectorAll('.btn');
+const ratingCard = document.querySelector('.ratings-container')
+const thankCard = document.querySelector('.thank-you-container')
+const submitButton = document.querySelector('rating')
+const rating = document.getElementById('rating')
+const ratingNumber = document.querySelectorAll('.btn')
 
 submitButton.addEventListener('click', () => {
     thankCard.classList.remove('hidden');
-    ratingCard.computedStyleMap.display = "none";
-});
+    ratingCard.computedStyleMap.display = "none"
+})
 
 ratingNumber.forEach((rates) => {
     rates.addEventListener('click', () => {
-        rating.innerHTML = rates.innerHTML;
-    });
-});
+        rating.innerHTML = rates.innerHTML
+    })
+})
